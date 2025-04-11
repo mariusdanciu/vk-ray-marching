519046e7cd8cd57388f009e069d9783c5c49c7cf
Marius Danciu <marius.danciu@gmail.com>
2025-04-11 12:57:36 +0300

    clean

--- src/main.rs
+++ src/main.rs
@@ -77,16 +77,8 @@ impl App {
     fn new(event_loop: &EventLoop<()>) -> Self {
         let library = VulkanLibrary::new().unwrap();
 
-        // The first step of any Vulkan program is to create an instance.
-        //
-        // When we create an instance, we have to pass a list of extensions that we want to enable.
-        //
-        // All the window-drawing functionalities are part of non-core extensions that we need to
-        // enable manually. To do so, we ask `Surface` for the list of extensions required to draw
-        // to a window.
         let required_extensions = Surface::required_extensions(event_loop).unwrap();
 
-        // Now creating the instance.
         let instance = Instance::new(
             library,
             InstanceCreateInfo {
@@ -99,63 +91,27 @@ impl App {
         )
         .unwrap();
 
-        // Choose device extensions that we're going to use. In order to present images to a
-        // surface, we need a `Swapchain`, which is provided by the `khr_swapchain` extension.
         let device_extensions = DeviceExtensions {
             khr_swapchain: true,
             ..DeviceExtensions::empty()
         };
 
-        // We then choose which physical device to use. First, we enumerate all the available
-        // physical devices, then apply filters to narrow them down to those that can support our
-        // needs.
-        let (physical_device, queue_family_index) = instance
+         let (physical_device, queue_family_index) = instance
             .enumerate_physical_devices()
             .unwrap()
             .filter(|p| {
-                // Some devices may not support the extensions or features that your application,
-                // or report properties and limits that are not sufficient for your application.
-                // These should be filtered out here.
                 p.supported_extensions().contains(&device_extensions)
             })
             .filter_map(|p| {
-                // For each physical device, we try to find a suitable queue family that will
-                // execute our draw commands.
-                //
-                // Devices can provide multiple queues to run commands in parallel (for example a
-                // draw queue and a compute queue), similar to CPU threads. This is
-                // something you have to have to manage manually in Vulkan. Queues
-                // of the same type belong to the same queue family.
-                //
-                // Here, we look for a single queue family that is suitable for our purposes. In a
-                // real-world application, you may want to use a separate dedicated transfer queue
-                // to handle data transfers in parallel with graphics operations.
-                // You may also need a separate queue for compute operations, if
-                // your application uses those.
                 p.queue_family_properties()
                     .iter()
                     .enumerate()
                     .position(|(i, q)| {
-                        // We select a queue family that supports graphics operations. When drawing
-                        // to a window surface, as we do in this example, we also need to check
-                        // that queues in this queue family are capable of presenting images to the
-                        // surface.
                         q.queue_flags.intersects(QueueFlags::GRAPHICS)
                             && p.presentation_support(i as u32, event_loop).unwrap()
                     })
-                    // The code here searches for the first queue family that is suitable. If none
-                    // is found, `None` is returned to `filter_map`, which
-                    // disqualifies this physical device.
                     .map(|i| (p, i as u32))
             })
-            // All the physical devices that pass the filters above are suitable for the
-            // application. However, not every device is equal, some are preferred over others.
-            // Now, we assign each physical device a score, and pick the device with the lowest
-            // ("best") score.
-            //
-            // In this example, we simply select the best-scoring device to use in the application.
-            // In a real-world setting, you may want to use the best-scoring device only as a
-            // "default" or "recommended" device, and let the user choose the device themself.
             .min_by_key(|(p, _)| {
                 // We assign a lower score to device types that are likely to be faster/better.
                 match p.properties().device_type {
@@ -176,21 +132,10 @@ impl App {
             physical_device.properties().device_type,
         );
 
-        // Now initializing the device. This is probably the most important object of Vulkan.
-        //
-        // An iterator of created queues is returned by the function alongside the device.
         let (device, mut queues) = Device::new(
-            // Which physical device to connect to.
             physical_device,
             DeviceCreateInfo {
-                // A list of optional features and extensions that our program needs to work
-                // correctly. Some parts of the Vulkan specs are optional and must be enabled
-                // manually at device creation. In this example the only thing we are going to need
-                // is the `khr_swapchain` extension that allows us to draw to a window.
                 enabled_extensions: device_extensions,
-
-                // The list of queues that we are going to use. Here we only use one queue, from
-                // the previously chosen queue family.
                 queue_create_infos: vec![QueueCreateInfo {
                     queue_family_index,
                     ..Default::default()
@@ -201,22 +146,15 @@ impl App {
         )
         .unwrap();
 
-        // Since we can request multiple queues, the `queues` variable is in fact an iterator. We
-        // only use one queue in this example, so we just retrieve the first and only element of
-        // the iterator.
         let queue = queues.next().unwrap();
 
         let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
-        // Before we can start creating and recording command buffers, we need a way of allocating
-        // them. Vulkano provides a command buffer allocator, which manages raw Vulkan command
-        // pools underneath and provides a safe interface for them.
         let command_buffer_allocator = Arc::new(StandardCommandBufferAllocator::new(
             device.clone(),
             Default::default(),
         ));
 
-        // We now create a buffer that will store the shape of our triangle.
         let vertices = [
             MyVertex {
                 position: [-1.0, -1.0],
